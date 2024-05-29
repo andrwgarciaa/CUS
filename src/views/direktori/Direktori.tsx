f1f315159bc08b8cb5bdfe6018877f1742e063a0
geraldpho <you@example.com>
2024-05-29 21:51:48 +0700

    baru heading di direktori :>

---
+++ src/views/direktori/Direktori.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../../components/Navbar"
+
+const Direktori = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className = "container mx-auto mt-10 px-4">
+                <header className="mb-8">
+                    <h1 className="judul font-bold">Direktori Tempat</h1>
+                </header>
+            </div>
+        </div>
+    )
+}
