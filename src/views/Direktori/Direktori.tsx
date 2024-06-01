55ce2d479f2e650c35b954a23dc775706f58dc0b
Andrew G <andrwgarciaa@gmail.com>
2024-06-02 01:16:13 +0700

    fix: merge error

--- src/views/Direktori/Direktori.tsx
+++ src/views/Direktori/Direktori.tsx
@@ -19,14 +19,19 @@ const Direktori = () => {
         <header className="mb-8 flex items-center">
           <div className="flex-1">
             <h1 className="text-2xl font-bold mb-2">Direktori Tempat</h1>
-            <p className="deskripsi max-w-xl break-words">
-              Lorem ipsum dolor sit amet consectetur. Volutpat aenean pretium
-              quam orci id semper viverra. Laoreet porta ut porta pulvinar.
-              Volutpat morbi tortor neque pellentesque quis. Vel purus bibendum
-              purus feugiat eu.
+            <p className="deskripsi max-w-2xl break-words text-justify">
+              Menjelajah ke tempat-tempat seperti restoran, kafe, mal,
+              supermarket, taman, hingga bengkel memberikan pengalaman yang
+              beragam, seperti menikmati kuliner lezat hingga berbelanja semua
+              kebutuhan anda, bersantai di alam, memperbaiki kendaraan, dan
+              menikmati kopi di suasana yang nyaman.
             </p>
           </div>
-          <div className="w-48 h-48 bg-gray-500 ml-8"></div>
+          <img
+            src="https://images.pexels.com/photos/2962035/pexels-photo-2962035.jpeg"
+            alt="Direktori Tempat"
+            style={{ width: "35%", height: "40%", objectFit: "cover" }}
+          />
         </header>
         {allPlaces?.map((place) => (
           <DirektoriSection
