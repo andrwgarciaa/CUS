cb6163c0abd565bf7cf2fb1318c67bd5743feae7
Andrew G <andrwgarciaa@gmail.com>
2024-06-12 14:56:51 +0700

    update: lihatsemua dinamis

--- src/views/Komunitas/views/LihatSemuaKomunitas/LihatSemuaKomunitas.tsx
+++ src/views/Komunitas/views/LihatSemuaKomunitas/LihatSemuaKomunitas.tsx
@@ -3,20 +3,27 @@ import { useParams } from "react-router-dom";
 import KomunitasCard from "../../components/KomunitasCard";
 import LoadingWithMessage from "../../../../components/LoadingWithMessage";
 import { ICommunityActivity } from "../../interfaces";
-import { getCommunityActivityByTypeId } from "../../utilities";
+import {
+  getCommunityActivityByTypeId,
+  getCommunityActivityCategoryById,
+} from "../../utilities";
+import { ICategory } from "../../../../interfaces";
+import { PLACE_CATEGORY_URL, PLACE_URL } from "../../../../constants";
 const LihatSemuaKomunitas = () => {
   const { id } = useParams<{ id: string }>();
   const [allCommunityActivity, setAllCommunityActivity] = useState<
     ICommunityActivity[] | null
   >([]);
   const [communityActivityType, setCommunityActivityType] =
-    useState<ICommunityActivity | null>();
+    useState<ICategory | null>();
   const [loading, setLoading] = useState<boolean>(true);
 
   const fetchData = async () => {
     setLoading(false);
     const data = await getCommunityActivityByTypeId(id);
+    const categoryData = await getCommunityActivityCategoryById(id);
     if (data.data) setAllCommunityActivity(data.data);
+    if (categoryData.data) setCommunityActivityType(categoryData.data);
   };
 
   useEffect(() => {
@@ -30,13 +37,13 @@ const LihatSemuaKomunitas = () => {
           <header className="mb-20 flex items-center">
             <div className="flex-1">
               <h1 className="text-3xl font-bold mb-2">
-                {/* {communityActivityType?.category} */}
+                {communityActivityType?.category}
               </h1>
               <p className="deskripsi max-w-3xl break-words text-justify">
-                {/* {communityActivityType?.description} */}
+                {communityActivityType?.description}
               </p>
             </div>
-            {/* <img
+            <img
               src={
                 communityActivityType?.has_photo
                   ? PLACE_CATEGORY_URL + communityActivityType?.id
@@ -45,7 +52,7 @@ const LihatSemuaKomunitas = () => {
               alt="Direktori Tempat"
               className="rounded-lg shadow-md"
               style={{ width: "35%", height: "40%", objectFit: "cover" }}
-            /> */}
+            />
           </header>
           <div
             style={{
