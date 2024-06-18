16801d55aa18b4da90bb6111ac59877a26e00a6e
Andrew G <andrwgarciaa@gmail.com>
2024-06-18 23:55:45 +0700

    update: map config to add min zoom and max bounds

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { IPlace } from "../../interfaces";
 import { getAllPlaces } from "../Direktori/utilities";
 import Map from "../../components/Map";
+import L from "leaflet";
 
 export default function Home() {
   const [selectedCategory, setSelectedCategory] = useState<number>(0);
@@ -23,6 +24,13 @@ export default function Home() {
       />
       <MapContainer
         center={[-6.2246, 106.6484]}
+        maxBounds={
+          new L.LatLngBounds(
+            [-6.208506970199381, 106.61883450655536],
+            [-6.272911805822472, 106.69310863096636]
+          )
+        }
+        minZoom={15}
         zoom={17}
         scrollWheelZoom={true}
       >
